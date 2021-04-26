d158c4b9641fce1fc88d1b22d94ae1ac37f2ed11
wasabi <wasabi@podbbang.com>
2021-04-26 18:41:02 +0900

    feat: 검색 기능 구현 중

--- src/app.jsx
+++ src/app.jsx
@@ -4,24 +4,41 @@ import VideoList from './components/video-list/videoList';
 
 function App() {
   const [videoList, setVideoList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const apiURL = "https://www.googleapis.com/youtube/v3/search";
+  let options = {
+    part: "id,snippet",
+    regionCode: "KR",
+    key: "AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU",
+    maxResults: 20,
+    q: searchKeyword,
+  };
+
+  const getVideoList = async () => {
+    const params = Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
+    const fetchData = await fetch(`${apiURL}?${params}`)
+      .then((res) => res.json())
+      .then((res) => res.items);
+
+      console.log(fetchData)
+    setVideoList(fetchData);
+  };
+  
+  
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      setSearchKeyword(e.target.value);
+      getVideoList();
+    }
+  };
 
   useEffect(() => {
-    const getVideoList = async () => {
-      const fetchData = await fetch(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&key=AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU&maxResults=20"
-      )
-        .then((res) => res.json())
-        .then((res) => res.items);
-
-      setVideoList(fetchData);
-    };
-
     getVideoList();
   }, []);
 
   return (
     <>
-      <Header></Header>
+      <Header handleSearch={handleSearch}></Header>
       <VideoList videoList={videoList}></VideoList>
     </>
   );
