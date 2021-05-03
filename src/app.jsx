9d7445e406914a70a797f75b41401c6ed4b15148
wasabi <wasabi@podbbang.com>
2021-05-03 16:26:22 +0900

    refactor: Change useEffect Hook
    - effect 내부에서만 사용하는 함수 effect 내부로 이동
    - effect에서 사용하는 state값인 keyword값을 의존성으로 주입
    
    [참고] https://overreacted.io/ko/a-complete-guide-to-useeffect/

--- src/app.jsx
+++ src/app.jsx
@@ -6,35 +6,34 @@ function App() {
   const [videoList, setVideoList] = useState([]);
   const [searchKeyword, setSearchKeyword] = useState('');
   const apiURL = "https://www.googleapis.com/youtube/v3/search";
-  let options = {
-    part: "id,snippet",
-    regionCode: "KR",
-    key: "AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU",
-    maxResults: 20,
-    q: searchKeyword,
-  };
-
-  const getVideoList = async () => {
-    const params = Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
-    const fetchData = await fetch(`${apiURL}?${params}`)
-      .then((res) => res.json())
-      .then((res) => res.items);
-
-      console.log(fetchData)
-    setVideoList(fetchData);
-  };
-  
   
   const handleSearch = (e) => {
     if (e.key === 'Enter') {
       setSearchKeyword(e.target.value);
-      getVideoList();
     }
   };
 
   useEffect(() => {
-    getVideoList();
-  }, []);
+      function getOptions() {
+        return {
+          part: "id,snippet",
+          regionCode: "KR",
+          key: "AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU",
+          maxResults: 20,
+          q: searchKeyword,
+        };
+      };
+
+      async function getVideoList() {
+        const options = getOptions();
+        const params = Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
+        const fetchData = await fetch(`${apiURL}?${params}`).then((res) => res.json()).then((res) => res.items); 
+
+        setVideoList(fetchData);
+      }
+
+      getVideoList();
+  }, [searchKeyword]);
 
   return (
     <>
