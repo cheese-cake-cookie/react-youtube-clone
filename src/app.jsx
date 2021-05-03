d3c2b8f4a8ff7c2e95de9254bc91e96c08d9f205
wasabi <wasabi@podbbang.com>
2021-05-03 16:39:14 +0900

    refactor: 함수를 컴포넌트 외부로 분리

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,18 @@ import {useState, useEffect} from 'react';
 import Header from './components/header/header';
 import VideoList from './components/video-list/videoList';
 
+function getSearchParams(searchKeyword) {
+  const options = {
+    part: "id,snippet",
+    regionCode: "KR",
+    key: "AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU",
+    maxResults: 20,
+    q: searchKeyword,
+  };
+
+  return Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
+};
+
 function App() {
   const [videoList, setVideoList] = useState([]);
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -14,21 +26,8 @@ function App() {
   };
 
   useEffect(() => {
-      function getOptions() {
-        return {
-          part: "id,snippet",
-          regionCode: "KR",
-          key: "AIzaSyBFZj-aYdpJJaUXvva_YKpoEDR91GVawzU",
-          maxResults: 20,
-          q: searchKeyword,
-        };
-      };
-
       async function getVideoList() {
-        const options = getOptions();
-        const params = Object.keys(options).map(key => `${key}=${options[key]}`).join('&');
-        const fetchData = await fetch(`${apiURL}?${params}`).then((res) => res.json()).then((res) => res.items); 
-
+        const fetchData = await fetch(`${apiURL}?${getSearchParams(searchKeyword)}`).then((res) => res.json()).then((res) => res.items); 
         setVideoList(fetchData);
       }
 
