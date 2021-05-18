5c1a78ae291e4d80ff976ebf0e3767355708a632
wasabi <wasabi@podbbang.com>
2021-05-18 15:12:50 +0900

    feat: videoDetail component 구현

--- src/components/video-detail/videoDetail.jsx
+++ src/components/video-detail/videoDetail.jsx
@@ -2,9 +2,16 @@ import styles from "./videoDetail.module.css";
 
 function VideoDetail({video}) {
   return (
-  <>
-  <h1>{video.title}</h1>
-  </>);
+  <div className={styles.video__detail}>
+    <img className={styles.video__thumbnail} src={video.thumbnails.high.url} alt={video.title} />
+    <section className={styles.video__info}>
+      <h1 className={styles.video__title}>{video.title}</h1>
+      <h2 className={styles.channel__title}>{video.channelTitle}</h2>
+      <hr/>
+      <span>게시 날짜: {video.publishedAt}</span>
+      <p className={styles.video__description}>{video.description}</p>
+    </section>
+  </div>);
 }
 
 export default VideoDetail;
